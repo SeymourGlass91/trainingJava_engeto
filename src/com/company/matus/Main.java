04d2946f5e95ba80cebc1b7027ac368c331b45c5
a_079170 <matusmolovcak1@gmail.com>
2022-02-04 12:42:51 +0100

    Engeto uloha 1

--- src/com/company/matus/Main.java
+++ src/com/company/matus/Main.java
@@ -35,10 +35,65 @@ public class Main {
 
         String ipAdresa = "191.168.1.1";
         System.out.println("Ip adresa klienta :  " + ipAdresa );
+        System.out.println("___________________________________________");
 
+        // Úkol: Oprav chybný kód
+        ukol1();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
 
+    }
 
+    public static void ukol1() {
+        System.out.println("Hello world!");
+    }
+    public static void ukol2() {
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+    }
 
+    public static void ukol3() {
+        int pocetLekci = 10;
+        System.out.println(pocetLekci);
+    }
 
+    public static void ukol4() {
+        double cena = 10.0;
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {
+        BigDecimal cena = new BigDecimal(0);
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1));
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+    }
+
+    public static void ukol6() {
+        BigDecimal cena = new BigDecimal(0);
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("Výkon Motoru je: " + vykonMotoru + " kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile + ".");
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
     }
 }
